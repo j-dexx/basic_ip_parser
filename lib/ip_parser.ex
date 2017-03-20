5fff98791de92f4be34b9f677d4febd47d9d0336
James Doyley <jdoyley@gmail.com>
2017-03-20 17:25:26 +0000

    first commit

---
+++ lib/ip_parser.ex
@@ -0,0 +1,18 @@
+defmodule IpParser do
+  @moduledoc """
+  Documentation for IpParser.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IpParser.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
